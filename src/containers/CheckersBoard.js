b9a8d68958bf196e3cfccf7593b05879f45f21fa
Dvessen83 <d.v.essen@gmail.com>
2017-04-12 15:49:58 +0200

    Changed to localhost

---
+++ src/containers/CheckersBoard.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+
+class CheckersBoard extends PureComponent {
+
+  render() {
+    return (
+      <div class = "checkers">
+      
+      </div>
+    )
+  }
+
+
+}
